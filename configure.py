acf75482428e3cd0aa5a5e9dbe0872e355cb1b37
nyyakko <nyakonyns@gmail.com>
2024-01-08 18:45:34 -0300

    python: first check before deleting.

--- configure.py
+++ configure.py
@@ -4,5 +4,7 @@ import shutil
 
 BUILD_TYPE = sys.argv[1] if len(sys.argv) >= 2 else 'debug'
 
-shutil.rmtree('build/')
+if (os.path.isdir('build/')):
+    shutil.rmtree('build/')
+
 os.system(f'cmake -DCMAKE_BUILD_TYPE={BUILD_TYPE} -G Ninja -B./build/ -S./')
