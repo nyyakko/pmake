235a3a987856092912d93780d6afe16f0dfba23a
nyyakko <nyakonyns@gmail.com>
2024-01-08 18:41:42 -0300

    python: remove ``build/`` to prevent issues with ``FetchContent``.

--- configure.py
+++ configure.py
@@ -1,6 +1,8 @@
 import os
 import sys
+import shutil
 
 BUILD_TYPE = sys.argv[1] if len(sys.argv) >= 2 else 'debug'
 
+shutil.rmtree('build/')
 os.system(f'cmake -DCMAKE_BUILD_TYPE={BUILD_TYPE} -G Ninja -B./build/ -S./')
